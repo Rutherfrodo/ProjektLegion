0ba23bd203f8902172f78572561d437065e97675
Rutherfrodo <rutherfrodo@gmail.com>
2019-11-14 19:47:25 +0100

    Dodanie rozwijanego paska bocznego

--- script/poradnik.js
+++ script/poradnik.js
@@ -53,3 +53,15 @@ function walka() {
   document.getElementById("technologie").style.display = "none";
   window.scrollBy(0, -1 * window.pageYOffset );
 }
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+} 
