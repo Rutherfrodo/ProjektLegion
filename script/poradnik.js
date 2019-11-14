8fcce4e55f8258b0f74ca16e2cb6a61227a8b08c
Rutherfrodo <rutherfrodo@gmail.com>
2019-11-14 18:08:07 +0100

    dodanie składu

--- script/poradnik.js
+++ script/poradnik.js
@@ -2,16 +2,25 @@ function menugry() {
   document.getElementById("menugry").style.display = "block";
   document.getElementById("dyplomacja").style.display = "none";
   document.getElementById("produkcja").style.display = "none";
+  document.getElementById("sklad").style.display = "none";
 }
 
 function dyplomacja() {
   document.getElementById("dyplomacja").style.display = "block";
   document.getElementById("menugry").style.display = "none";
   document.getElementById("produkcja").style.display = "none";
+  document.getElementById("sklad").style.display = "none";
 }
 function produkcja() {
   document.getElementById("produkcja").style.display = "block";
-  document.getElementById("dyplomacja").style.display = "none";
   document.getElementById("menugry").style.display = "none";
+  document.getElementById("dyplomacja").style.display = "none";
+    document.getElementById("sklad").style.display = "none";
+}
+function sklad() {
+  document.getElementById("sklad").style.display = "block";
+  document.getElementById("menugry").style.display = "none";
+  document.getElementById("dyplomacja").style.display = "none";
+  document.getElementById("produkcja").style.display = "none";
 
 }
