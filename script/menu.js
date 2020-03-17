76c4e3cdc0b1a32722938c05108fd07d56b68cf4
Rutherfrodo <rutherfrodo@gmail.com>
2020-03-17 15:40:41 +0100

    Color change in navbar

--- script/menu.js
+++ script/menu.js
@@ -4,10 +4,10 @@ function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.padding = "30px 10px";
     document.getElementById("logo").style.fontSize = "25px";
-    document.getElementById("navbar").style.background ="#7e0505";
+    document.getElementById("navbar").style.background ="#580303";
   } else {
     document.getElementById("navbar").style.padding = "80px 10px";
     document.getElementById("logo").style.fontSize = "35px";
-    document.getElementById("navbar").style.background ="#7e0505 url(spqr2.png)";
+    document.getElementById("navbar").style.background ="#580303 url(spqr2.png)";
   }
 }
