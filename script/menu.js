9d7b51bf5a969cf5ee4bb906404189cda443808d
Rutherfrodo <rutherfrodo@gmail.com>
2020-03-17 14:50:23 +0100

    Adding img to navbar

--- script/menu.js
+++ script/menu.js
@@ -4,8 +4,10 @@ function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.padding = "30px 10px";
     document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("navbar").style.background ="#7e0505";
   } else {
     document.getElementById("navbar").style.padding = "80px 10px";
     document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("navbar").style.background ="#7e0505 url(spqr2.png)";
   }
 }
