3b06df2c56dadc721f5a0ad28209d58999e736f1
Rutherfrodo <rutherfrodo@gmail.com>
2020-03-18 12:02:21 +0100

    Removing img from header navbar adding functions to navbar buttons

--- script/menu.js
+++ script/menu.js
@@ -4,12 +4,10 @@ function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.padding = "30px 10px";
     document.getElementById("logo").style.fontSize = "25px";
-    document.getElementById("navbar").style.background ="#580303";
     document.getElementById("turbo").style.display = "none";
   } else {
     document.getElementById("navbar").style.padding = "80px 10px";
     document.getElementById("logo").style.fontSize = "35px";
-    document.getElementById("navbar").style.background ="#580303 url(spqr2.png)";
     document.getElementById("turbo").style.display = "block";
   }
 }
--- script/poradnik.js
+++ script/poradnik.js
@@ -1,4 +1,45 @@
-function menugry() {
+var menu = document.getElementById("menugry");
+var dyplo = document.getElementById("dyplomacja");
+var prod = document.getElementById("produkcja");
+var sklad = document.getElementById("sklad");
+var rese = document.getElementById("technologie");
+var war = document.getElementById("walka");
+function removingnavbargap(){
+  window.scrollBy(0,-100);
+}
+ 
+
+function menugry(){
+  menu.scrollIntoView();
+  window.scrollBy(0,-150);
+}
+
+function dyplomatic(){
+  dyplo.scrollIntoView();
+  removingnavbargap();
+}
+
+function production(){
+prod.scrollIntoView();
+removingnavbargap();
+}
+
+function sklady(){
+  sklad.scrollIntoView();
+  removingnavbargap();
+}
+
+function resea(){
+  rese.scrollIntoView();
+  removingnavbargap();
+}
+
+function wartaktics(){
+  war.scrollIntoView();
+  removingnavbargap();
+}
+
+/*function menugry() {
   document.getElementById("menugry").style.display = "block";
   document.getElementById("dyplomacja").style.display = "none";
   document.getElementById("produkcja").style.display = "none";
@@ -6,6 +47,8 @@ function menugry() {
   document.getElementById("technologie").style.display = "none";
   document.getElementById("walka").style.display = "none";
   window.scrollBy(0, -1 * window.pageYOffset ); //scroll do gory po zmianie karty
+
+
 }
 
 function dyplomacja() {
